bc1062fcaa53d2d285e13239744c666e487668ba
tchibindi <tapiwa.chibindi@cellulant.io>
2022-07-21 13:55:35 +0200

    :alembic: Adding swagger to the project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello/')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('hello/:term')
+  public sampleRoute(@Param('term') term: string): string {
+    return `your term: ${term}`;
   }
 }
